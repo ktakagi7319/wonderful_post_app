7f908d51402081726feb195f5a25fef96f638c52
高木健吾 <takagiastro@gmail.com>
2022-06-25 23:19:16 +0900

    Task7.CRUD setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'articles#index'
+  resources :articles
+  resources :sample_articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
